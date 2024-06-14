01534b874e5f7e7006829e6401ec3d9fa646800b
suhas <suhas.malakala@gmail.com>
2024-06-14 18:55:48 +0530

    auth.ts updated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,6 +21,13 @@ export const authConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   callbacks: {
